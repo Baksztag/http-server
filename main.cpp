cc4a4ae6682e72f20bde1204d00d1cbadaf5ae5a
Jakub Kręt <jkret@Jakubs-MacBook-Air.local>
2017-05-30 21:33:57 +0200

    Move setup methods to separate functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <sys/socket.h>
-#include <cstring>
 #include <netdb.h>
 #include <unistd.h>
 
@@ -17,7 +16,7 @@
 //W prezentacji zaprezentowac dzialajaca obsluge bledow
 //oraz w POST obsluzyc skrypt CGI
 
-int main() {
+int set_up_server_socket(char *port) {
     struct addrinfo name, *res;
     int server_socket;
     memset(&name, 0, sizeof(name));
@@ -27,32 +26,51 @@ int main() {
     getaddrinfo(NULL, "8000", &name, &res);
     if ((server_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0) {
         printf("An error occurred while trying to create socket\n");
-        exit(EXIT_FAILURE);
+        return -1;
     }
     if ((bind(server_socket, res->ai_addr, res->ai_addrlen)) < 0) {
         printf("An error occurred while trying to bind the socket to port\n");
         shutdown(server_socket, SHUT_RDWR);
         close(server_socket);
-        exit(EXIT_FAILURE);
+        return -1;
     }
     if ((listen(server_socket, 5)) < 0) {
         printf("An error occurred while trying to start listening\n");
         shutdown(server_socket, SHUT_RDWR);
         close(server_socket);
-        exit(EXIT_FAILURE);
+        return -1;
     }
+    return server_socket;
+}
+
+int accept_connection(int server_socket) {
     sockaddr_storage address;
     socklen_t address_len = sizeof(address);
     int client_socket;
     if ((client_socket = accept(server_socket, (sockaddr *) &address, &address_len)) < 0) {
         perror("Accept");
-        shutdown(server_socket, SHUT_RDWR);
-        close(server_socket);
+        return -1;
+    }
+    return client_socket;
+}
+
+int main() {
+    char *port = (char *) "8000";
+    int server_socket;
+    if ((server_socket = set_up_server_socket(port)) < 0) {
+        printf("An error occurred while trying to set up server socket\n");
         exit(EXIT_FAILURE);
     }
-    char request[1024];
-    recv(client_socket, request, 1024, 0);
-    printf("Request from client: %s\n", request);
+
+    printf("Waiting for connection\n");
+    int client_socket;
+    if ((client_socket = accept_connection(server_socket)) < 0) {
+        printf("An error occurred while trying to accept a connection\n");
+    }
+
+    char req[1024];
+    recv(client_socket, req, 1024, 0);
+    printf("Request from client: %s\n", req);
     send(client_socket, "abc", 1024, 0);
 
     shutdown(server_socket, SHUT_RDWR);
